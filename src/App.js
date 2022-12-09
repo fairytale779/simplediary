7012eec11176ed90328dbba95fbb1efe8c8f3e0f
Donghwa <hhhhong44@gmail.com>
2022-12-09 17:54:20 +0900

    json자료들 Data로 출력

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-import Lifecycle from './lifecycle'
+// import Lifecycle from './lifecycle'
 
 // const dummyList = [
 //   {
@@ -42,10 +42,33 @@ import Lifecycle from './lifecycle'
 //   },
 // ]
 
+// https://jsonplaceholder.typicode.com/comments
+
+
 function App() {
   const [data, setData] = useState([])
 
   const dataId = useRef(0)
+
+  const getData = async () => { 
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res) => res.json());
+
+    const initData = res.slice(0,20).map((it)=>{
+      return  {
+        author : it.email,
+        content : it.body,
+        emotion : Math.floor(Math.random()*5) + 1, // Math.random()*5 : 0~4까지의 수를 랜덤으로 하나 생성(소수점포함) -> floor : 정수반환 -> +1 : 1~5
+        created_date : new Date().getTime(),
+        id : dataId.current++ 
+      }
+    })
+  
+    setData(initData)
+  }
+
+  useEffect (()=>{
+    getData()
+  },[])
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime()
@@ -81,7 +104,7 @@ function App() {
 
   return (
     <div className="App">
-      <Lifecycle />
+      {/* <Lifecycle /> */}
       <DiaryEditor onCreate={onCreate}/>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data}/>
     </div>
